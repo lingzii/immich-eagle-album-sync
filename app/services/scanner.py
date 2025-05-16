3703c0a435338637d94dbc5418c47f9472fde512
lingzii <lingziha@gmail.com>
2025-05-16 10:59:07 +0800

    ✨ feat: Refactor start_sync_scanner to improve structure and add folder handling logic

--- app/services/scanner.py
+++ app/services/scanner.py
@@ -1,5 +1,6 @@
 import logging
 import math
+import re
 
 import anyio
 import httpx
@@ -9,9 +10,9 @@ logger = logging.getLogger(__name__)
 
 
 class EagleAsyncClient(httpx.AsyncClient):
-    def __init__(self, **kwargs):
+    def __init__(self, *, base_url: str = "", **kwargs):
         self.token = {"token": settings.EAGLE_API_KEY}
-        super().__init__(base_url=settings.EAGLE_API_URL, **kwargs)
+        super().__init__(base_url=base_url, **kwargs)
 
     async def get(self, url, *, params=None, **kwargs):
         merged_params = {**self.token, **(params or {})}
@@ -44,20 +45,33 @@ async def fetch_all_assets(client: httpx.AsyncClient, size: int = 1000) -> list[
 
 
 async def start_sync_scanner():
-    while True:
-        try:
-            logger.info("Starting API scan for data changes...")
-            async with (
-                httpx.AsyncClient(
-                    base_url=settings.IMMICH_API_URL,
-                    headers={"x-api-key": settings.IMMICH_API_KEY},
-                ) as immich,
-                EagleAsyncClient() as eagle,
-            ):
-                pass
+    async with (
+        EagleAsyncClient(
+            base_url=settings.EAGLE_API_URL + "/api",
+        ) as eagle,
+        httpx.AsyncClient(
+            base_url=settings.IMMICH_API_URL + "/api",
+            headers={"x-api-key": settings.IMMICH_API_KEY},
+        ) as immich,
+    ):
+        while True:
+            try:
+                logger.info("Starting API scan for data changes...")
 
-        except Exception as e:
-            logger.exception(f"Error occurred during API scan: {e}")
+                # Get default folders and album folders
+                default_folders, eagle_albumId_relate_name = {}, {}
+                res = await eagle.get("/folder/list")
+                for i in res.json()["data"]:
+                    if re.match(r"^(1\d{3}|2\d{3})$", i["name"]):
+                        default_folders[i["name"]] = i["id"]
+                        for j in i["children"]:
+                            eagle_albumId_relate_name[j["id"]] = j["name"]
 
-        finally:
-            await anyio.sleep(settings.SCAN_INTERVAL)
+                immich_assets = {i["id"]: i for i in await fetch_all_assets(immich)}
+                logger.info(f"Fetched {len(immich_assets)} assets from Immich.")
+
+            except Exception as e:
+                logger.exception(f"Error occurred during API scan: {e}")
+
+            finally:
+                await anyio.sleep(settings.SCAN_INTERVAL)
